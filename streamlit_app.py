609745ac019fd1444db896f458abd685560e8707
crairobishawtrain <124177912+crairobishawtrain@users.noreply.github.com>
2023-02-01 16:14:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.heading('Breakfast Menu');
-Omega3 & Bl;ueberry Oatmeal
+streamlit.heading('Breakfast Menu')
+streamlit.text('Omega3 & Bl;ueberry Oatmeal')
